3885dfea53aace0d8d8421b60b3f5d8321c0181b
iabwaheed <ab.waheedkashmiri@gmail.com>
2023-01-22 23:15:39 +0500

    last update of layout

---
+++ app.js
@@ -0,0 +1,27 @@
+let aboutLink = document.getElementById("about-link")
+let popularLink = document.getElementById("popular-link")
+let menuLink = document.getElementById("menu-link")
+let contactLink = document.getElementById("contact-link")
+
+let about = document.getElementById("about-section")
+let popular = document.getElementById("popular-items")
+let menuSection = document.getElementById("menu-items")
+let contact = document.getElementById("footer")
+
+aboutLink.addEventListener("click", function () {
+    about.scrollIntoView(true)
+
+})
+
+popularLink.addEventListener("click", function() {
+    popular.scrollIntoView(true)
+})
+menuLink.addEventListener("click", function() {
+    menuSection.scrollIntoView(true)
+})
+contactLink.addEventListener("click", function() {
+    contact.scrollIntoView(true)
+})
+
+
+
